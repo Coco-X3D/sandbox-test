57bff83211e07208865956a8afb35a9d79c42dee
Coco <weixuankou@outlook.com>
2025-09-12 00:18:57 +0800

    create_common_word_list

--- main_q1.py
+++ main_q1.py
@@ -1,8 +1,31 @@
 import numpy as np 
 
 
-def test():
-    return
+# Let's first select a common 5-word list, since the first step is to test whether the input is a English word, only valid word input counts for one attempt
+def load_word_list():
+    word_list = np.loadtxt('./common_words.txt', dtype=str)
+    word_list_lower = np.char.lower(word_list)
+    mask = np.array([len(word) == 5 and word.isalpha() for word in word_list_lower])
+    five_letter_words = word_list_lower[mask]
+    return five_letter_words
+
+
+def is_5_english_word(word, word_list):
+    word = word.lower()
+    print(word.isalpha())
+    if len(word) != 5 or not word.isalpha() or not in word_list:
+        raise ValueError("Input word must be a 5-letter English word")
+    return word in word_list
+
+# def test():
+    # return
 def __main__():
-    a = np.array([1, 2, 3])
-    print(a)
+    print("This is main_q1.py")
+    a = load_word_list()
+    b = is_5_english_word("apsle", a)
+    print(b)
+
+
+
+if __name__ == '__main__':
+    __main__()
